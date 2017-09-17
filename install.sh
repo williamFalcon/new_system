01889e22f5afcda4103d133167cf24a38efbf48b
William Falcon <waf2107@columbia.edu>
2017-09-17 13:06:10 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,7 +9,12 @@ sudo apt-get install vim-nox
 # move .vim to home dir
 cd ..
 mv vim ~/.vim
-cd vim
+cd .vim
+
+# copy Vundle 
+git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 # copy vimrc
 mv .vimrc ~/.vimrc
+
+echo 'now open vim and run :PluginInstall'   
