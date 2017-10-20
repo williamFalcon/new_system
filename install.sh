391abbe17fd649d5811671727a94017da6ba615f
William Falcon <waf2107@columbia.edu>
2017-10-20 12:48:42 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -17,4 +17,19 @@ git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 # copy vimrc
 mv .vimrc ~/.vimrc
 
+# ----------------------
+# INSTALL YOU COMPLETE ME (YCM) VIM AUTOCOMPLETE
+# ----------------------
+# Install YCM (instructions: https://github.com/Valloric/YouCompleteMe/blob/master/README.md#ubuntu-linux-x64)
+# Install development tools and CMake:
+sudo apt-get install build-essential cmake
+
+# Make sure you have Python headers installed:
+sudo apt-get install python-dev python3-dev
+
+# Compiling YCM with semantic support for C-family languages:
+cd ~/.vim/bundle/YouCompleteMe
+./install.py --clang-completer
+
+
 echo 'now open vim and run :PluginInstall'   
